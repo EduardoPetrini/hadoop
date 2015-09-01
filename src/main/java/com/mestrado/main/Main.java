10243ad2952b2c8ef6b7e84aa01c9bdb1e5045c6
Eduardo Petrini <eduardo.petrini@outlook.com>
2015-09-01 09:59:57 -0300

    Alterado nome de usuário

--- src/main/java/com/mestrado/main/Main.java
+++ src/main/java/com/mestrado/main/Main.java
@@ -44,7 +44,7 @@ public class Main {
     public static double supportPercentage = 0.01;
     public static String support;
     public static int k = 1;
-    public static String user = "/user/eduardo/";
+    public static String user = "/user/hdp/";
     public static String inputEntry = "input/";
     public static String inputFileName = "";
     public static String clusterUrl = "hdfs://master/";
