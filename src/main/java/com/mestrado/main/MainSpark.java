29a13d81fe3ee45bce345a8fe473a9606234cf0c
Eduardo Petrini <eduardo.petrini@outlook.com>
2016-06-22 16:01:29 -0300

    Pause count itemsets

--- src/main/java/com/mestrado/main/MainSpark.java
+++ src/main/java/com/mestrado/main/MainSpark.java
@@ -304,7 +304,7 @@ public class MainSpark implements Serializable {
 		countDir++;
 		m.job1();
 //		m.job2();
-		StringBuilder log = new StringBuilder(CountItemsets.countItemsets(outputDirsName));
+		StringBuilder log = new StringBuilder("");
 		showTotalTime(log);
 //		CountItemsets.printRealItemsets();
 	}
